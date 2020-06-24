fefa22e0ed6340039a9ccedfee2dd1867e1b55fb
dalabarda <dalabarda@gmail.com>
2020-06-24 12:27:11 +0200

    1 file modified, 2 added

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -20,6 +20,8 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p>Continue ti Checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   )
 };
