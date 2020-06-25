dc645b63055a152a580abe155c690175647150a5
dalabarda <dalabarda@gmail.com>
2020-06-25 16:51:54 +0200

    1 file modified

--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
 
 import './SideDrawer.css';
 
 const sideDrawer = ( props ) => {
   // ...
   return (
-    <div className="SideDrawer">
-      <div className="Logo">
-        <Logo />
+    <Aux>
+      <Modal />
+      <div className="SideDrawer">
+        <div className="Logo">
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
 )};
 
 export default sideDrawer; 
