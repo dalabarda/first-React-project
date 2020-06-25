f31b5dada2016a3fca8aa4c7cbc7eb4a9c7dffd6
dalabarda <dalabarda@gmail.com>
2020-06-25 15:58:37 +0200

    1 file modified, 5 added, 1 deleted

--- src/components/Navigation/Toolbar/Toolbar.tsx
+++ src/components/Navigation/Toolbar/Toolbar.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import Logo from '../../Logo/Logo';
 import './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
 
 const toolbar = ( props ) => (
   <header className="Toolbar">
     <div>MENU </div>
     <Logo />
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 );
 
 export default toolbar; 
+
+
+// 
