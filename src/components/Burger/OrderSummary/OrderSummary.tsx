9f1f2ddccf384e9e169595ce06ab39e896188925
dalabarda <dalabarda@gmail.com>
2020-07-08 16:14:15 +0200

    2 files modified

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter, RouteProps } from 'react-router';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
+// this interface is so stupid, but it can solve this problem.
+export interface Props {
+  match: {
+    params: {
+      id: string
+    }
+  }
+}
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+interface State {
+  id: string;
+}
+
+class OrderSummary extends React.Component<Props & RouteProps, State> {
+  
+  componentDidUpdate() {
+    console.log('[OrderSummary] DidUpdate')
+  }
+  
+  render() {
+    const ingredientSummary = Object.keys( this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
           <span style={{textTransform: 'capitalize'}}>{igKey}</span>
-          : { props.ingredients[igKey] }
+          : { this.props.ingredients[igKey] }
         </li>);
-    });
+    } );
 
-  return (
-    <Aux>
+    return (
+      <Aux>
       <h3> Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p>TOTAL PRICE: { props.price.toFixed(2) }</p>
+      <p>TOTAL PRICE: { this.props.price.toFixed(2) }</p>
       <p>Continue ti Checkout?</p>
-      <Button btnType="Danger"  clicked={props.purchaseCancelled}> CANCEL </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}> CONTINUE </Button>
+      <Button btnType="Danger"  clicked={this.props.purchaseCancelled}> CANCEL </Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}> CONTINUE </Button>
     </Aux>
-  )
-};
+    )
+  }
+}
 
-export default orderSummary
+export default OrderSummary
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -4,18 +4,35 @@ import './Modal.css'
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div className="Modal"
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
 
-export default modal;
+
+class Modal extends React.Component<Props, State>  {
+  
+  // important method to enhance performance and not update this component when other 
+  // components are also updated. 
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentDidUpdate() {
+    console.log('[modal] will update')
+  }
+
+  render() {
+    return (
+    <Aux>
+      <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+      <div className="Modal"
+        style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1': '0'
+        }}
+      >
+        {this.props.children}
+      </div>
+    </Aux>
+    )
+  }
+}
+
+export default Modal;
